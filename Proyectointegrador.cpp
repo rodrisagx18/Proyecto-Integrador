b21c3fbee42959789db3cf74c63bf06c55bbf3ac
rodrisagx17 <116994853+rodrisagx17@users.noreply.github.com>
2024-05-20 23:25:56 -0600

    Update Proyectointegrador.cpp

--- Proyectointegrador.cpp
+++ Proyectointegrador.cpp
@@ -5,9 +5,9 @@
 int main(){
 
     //Definimos las variables que nesesitaremos mas adelante
-    float h, lad,volumen;
+    float x,x1,y,P,Vol, Volmax;
 	
-	char opcion;//declaro variables de entrada por el momento
+    char opcion;//declaro variables de entrada por el momento
 	setlocale(LC_ALL, "spanish");//defino el idioma del programa a español
 	do{
 	   
@@ -28,21 +28,30 @@ int main(){
 	
 	    switch(opcion){
 		    case 'A':
-			    printf("La formula al momento es V(x) = x^2(p-2x) * h\n");
+			    printf("La función que expresa el volumen del paquete es V(x) = Px^2-2x^3\n");
 		    break;
 		    case 'B':
                 //Le pedimos al usuario los datos y los leemos
-			    printf("Ingrese la altura de su prisma:\n");
-                scanf("%f",&h);
+		printf("Ingrese el valor de P:\n");
+                scanf("%f",&P);
+                //solicito x para el area de la base
+                printf("\nIngrese el valor de x:\n");
+                scanf("%f",&x);
 
-                printf("\nIngrese la longitud de un lado de su base:\n");
-                scanf("%f",&lad);
-
-                //Calculamos el volumen
-                volumen = (h*h)*(lad-(2*h));
-
+                //Calculamos el valor de y
+                y=P-2*x;
+                //calculamos el volumen
+	        Vol=x*x*y;
+		//valor de x que maximiza la función del volumen x^2(P-2x)
+                x1=P/3;
+		//valor maximo de la función de volumen    
+		Volmax=x1*x1*(P-2*x1);
+	        //condición para el volumen maximo de uno de sus lados
+		if(Volmax==P*P*P/27){
+			printf("Volumen maximo")
+		}
                 //Le imprimimos el resultado
-                printf("El volumen de su prisma es: %0.2f", volumen);
+                printf("El volumen de su prisma es: %0.2f", Vol);
 		    break;
 		    case 'C':
 			    printf("Abir grafica de phyton\n");
