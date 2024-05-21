a66f354bdb92c6e54a3c6ecde782dff065562760
JaProy <98243876+JaProy@users.noreply.github.com>
2024-05-21 12:13:36 -0600

    Update ProyectoIntegradorFinalGraficaPhyton.py

--- ProyectoIntegradorFinalGraficaPhyton.py
+++ ProyectoIntegradorFinalGraficaPhyton.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 p = float(input("Ingrese el valor de p: "))
-x = float(input("Ingrese el valor de x: "))
-x2 = float
-p3 = float
+x1 = float(input("Ingrese el valor de x: "))
+#x2 = float
+#p3 = float
 
 
-x2 = x ** 2
-p3 = p ** 3
+#x2 = x1 ** 2
+#p3 = p ** 3
 
 x = np.arange(-30,30,0.5)  # Le en una estructura, los valores que tendra la grafica en Y
-y = y = p * x2 - 2 * p3
+y = p * (x * 2) - 2 * (x * 3)
 
 plt.plot(x, y)
 plt.show()
