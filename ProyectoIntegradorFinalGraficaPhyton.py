6e8b052beb12a1803c9daf97c46a2ab9d66d2c4c
JaProy <98243876+JaProy@users.noreply.github.com>
2024-05-21 08:34:08 -0600

    Update ProyectoIntegradorFinalGraficaPhyton.py

--- ProyectoIntegradorFinalGraficaPhyton.py
+++ ProyectoIntegradorFinalGraficaPhyton.py
@@ -3,15 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Definimos Variables
-l = 5.0
-h = 19.0
+p = float(input("Ingrese el valor de p: "))
+x = float(input("Ingrese el valor de x: "))
+x2 = float
+p3 = float
 
 #Elevamos la altura al cuadrado
-hh = h ** 2
+x2 = x ** 2
+p3 = p ** 3
 
 #Le asignamos valores a X y Y
 x = np.arange(-10,10,.5)   #Le en una estructura, los valores que tendra la grafica en Y
-y = x*hh*l-2*h #Formula que utilizara para X
+y = (p(x2))-2(p3)#Formula que utilizara para X
 
 #Imprimimos la grafica en una nueva ventana!
 plt.plot(x,y)
