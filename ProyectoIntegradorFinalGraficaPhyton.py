8b9aa253e1339e795a5fa4fa2cd1e85a68eae5e7
JaProy <98243876+JaProy@users.noreply.github.com>
2024-05-22 23:51:47 -0600

    Update ProyectoIntegradorFinalGraficaPhyton.py
    123

--- ProyectoIntegradorFinalGraficaPhyton.py
+++ ProyectoIntegradorFinalGraficaPhyton.py
@@ -1,17 +1,24 @@
+#Importamos librerias
 import matplotlib.pyplot as plt
 import numpy as np
 
-p = float(input("Ingrese el valor de p: "))
+#Definimos las variables a utilizar, tambien pedimos por la entrada de datos su valor
 x1 = float(input("Ingrese el valor de x: "))
-#x2 = float
-#p3 = float
+y1 = float(input("Ingrese el valor de y: "))
+p = float
 
+#Definimos P
+P = 4 * x1
 
-#x2 = x1 ** 2
-#p3 = p ** 3
+#Definimos x, y, para que nuestra grafica las aplique 
+x = np.arange(-30,30,0.5)
+y = p * (x ** 2) - 2 * (x ** 3)
 
-x = np.arange(-30,30,0.5)  # Le en una estructura, los valores que tendra la grafica en Y
-y = p * (x * 2) - 2 * (x * 3)
+#Le damos nombre a la grafica y a las etiquetas de x,y
+plt.title("Grafíco de la funcion V(x) = Px^2 - 2x^3")
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
 
+#Mostramos la tabla
 plt.plot(x, y)
 plt.show()
