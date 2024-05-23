b2231c4ee58d24637e50b3b480adafe4aec74327
JaProy <98243876+JaProy@users.noreply.github.com>
2024-05-23 09:19:29 -0600

    Update ProyectoIntegradorFinalGraficaPhyton.py
    123

--- ProyectoIntegradorFinalGraficaPhyton.py
+++ ProyectoIntegradorFinalGraficaPhyton.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Definimos las variables a utilizar, tambien pedimos por la entrada de datos su valor
-x1 = float(input("Ingrese el valor de x: "))
-y1 = float(input("Ingrese el valor de y: "))
+x = float(input("Ingrese el valor de x en cm: "))
+y1 = float(input("Ingrese el valor de y en cm: "))
 p = float
 
 #Definimos P
-P = 4 * x1
+P = 4 * x
+#P = x1 + x1 + y1
 
 #Definimos x, y, para que nuestra grafica las aplique 
 x = np.arange(-30,30,0.5)
-y = p * (x ** 2) - 2 * (x ** 3)
+y = P * (x ** 2) - 2 * (x ** 3)
 
 #Le damos nombre a la grafica y a las etiquetas de x,y
 plt.title("Grafíco de la funcion V(x) = Px^2 - 2x^3")
